3b4d15893a6802f8359aa8e8c6e143ba5fd8301f
haha0511 <j.i.n.1.0.0.5.1.1wj@gmail.com>
2026-07-05 16:43:50 +0900

    Update game.js

--- game/game.js
+++ game/game.js
@@ -4,6 +4,18 @@
 // Part 1
 // ==========================================
 
+const bgm = new Audio("../sound/bgm.mp3");
+
+bgm.loop = true;
+
+bgm.volume = 0.3;
+
+window.addEventListener("click", () => {
+
+    bgm.play();
+
+}, { once: true });
+
 import { db } from "../firebase.js";
 
 import {
