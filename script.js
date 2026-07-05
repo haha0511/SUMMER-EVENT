952359a45df9192814e9f08b6fe80eca29ec2066
haha0511 <j.i.n.1.0.0.5.1.1wj@gmail.com>
2026-07-05 13:28:00 +0900

    Refactor script.js for improved readability

--- script.js
+++ script.js
@@ -14,42 +14,22 @@ if (shellPoint === null) {
 // 화면 표시
 document.getElementById("shellPoint").textContent = shellPoint;
 
-// =============================
 // 게임
-// =============================
-
 document.getElementById("gameObject").addEventListener("click", () => {
-
     location.href = "game/game.html";
-
 });
 
-// =============================
 // 상점
-// =============================
-
 document.getElementById("shopObject").addEventListener("click", () => {
-
     location.href = "shop/shop.html";
-
 });
 
-// =============================
 // 공지
-// =============================
-
 document.getElementById("noticeObject").addEventListener("click", () => {
-
     location.href = "notice/notice.html";
-
 });
 
-// =============================
 // 랭킹
-// =============================
-
 document.getElementById("rankObject").addEventListener("click", () => {
-
     location.href = "rank/rank.html";
-
 });
